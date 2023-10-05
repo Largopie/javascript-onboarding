ef02f02286d9225056aacb743805bf3bb9a061b1
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-05 20:22:51 +0900

    Feat 1: problem1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,31 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [pobiLeft, pobiRight] = pobi;
+  const [crongLeft, crongRight] = crong;
+
+  if(pobiLeft === 1 || pobiRight === 400) return -1;
+  if(crongLeft === 1 || crongRight === 400) return -1;
+
+  if(pobiLeft + 1 !== pobiRight || crongLeft + 1 !== crongRight) return -1;
+
+  const returnMax = (num) => {
+    const num_arr = String(num).split('').map((item) => Number(item));
+    
+    const num_sum = num_arr.reduce((sum, item) => sum + item);
+    const num_mul = num_arr.reduce((sum, item) => sum * item);
+
+    console.log(num_sum, num_mul);
+
+    return Math.max(num_sum, num_mul);
+  }
+
+  const pobiMax = Math.max(returnMax(pobiLeft), returnMax(pobiRight));
+  const crongMax = Math.max(returnMax(crongLeft), returnMax(crongRight));
+
+  if(pobiMax > crongMax) return 1;
+  else if(pobiMax < crongMax) return 2;
+  else if(pobiMax === crongMax) return 0;
 }
+
+problem1([97,98], [197,198])
 
 module.exports = problem1;
