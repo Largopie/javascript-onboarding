eb4734662b7e1a7cea4db009a14cf1b4621db625
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-06 17:07:30 +0900

    Feat: problem4 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,32 @@
+const createDictionary = (lowerCaseList, upperCaseList) => {
+  const dictionary = {};
+  for(let i = 0; i < lowerCaseList.length; i++) {
+    dictionary[lowerCaseList[i]] = lowerCaseList[lowerCaseList.length -1 - i];
+    dictionary[upperCaseList[i]] = upperCaseList[upperCaseList.length - 1 - i];
+  }
+
+  return dictionary;
+}
+
+const createAlphabetList = (startIndex) => {
+  const alphabetList = Array.from({length: 26}, (value, index) => String.fromCharCode(index + startIndex));
+
+  return alphabetList;
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const upperCaseAlphabetList = createAlphabetList(65);
+  const lowerCaseAlphabetList = createAlphabetList(97);
+
+  const treeFrogDictionary = createDictionary(lowerCaseAlphabetList, upperCaseAlphabetList);
+  let reverseWord = '';
+
+  for(char of word) {
+    if(treeFrogDictionary[char]) reverseWord+=treeFrogDictionary[char]
+    else reverseWord+=char;
+  }
+
+  return reverseWord;
 }
 
 module.exports = problem4;
