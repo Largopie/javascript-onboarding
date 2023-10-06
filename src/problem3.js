fde750c0ff6eebd893f8e5cf1cf8bbecbfc34c27
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-06 12:27:58 +0900

    refactor: 함수명 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,11 @@
-const findNumber = (num) => {
+const countClap = (num) => {
   const checkNum = ['3', '6', '9'];
   let count = 0;
-  const strNum = String(num);
-  for(char of strNum) {
-    if(checkNum.includes(char)){
-      count++;
-    }
+
+  for(char of String(num)) {
+    if(checkNum.includes(char)) count++;
   }
+
   return count;
 }
 
@@ -14,8 +13,9 @@ function problem3(number) {
   let answer = 0;
 
   for(let i =1; i <= number; i++) {
-    answer+=findNumber(i)
+    answer+=countClap(i)
   }
+
   return answer;
 }
 
