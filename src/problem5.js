04c92daadc19d9e8a7e931e8b55070c4e1e3f180
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-07 17:17:39 +0900

    Feat: problem5 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,20 @@
+const withdraw = (money, changeMoney) => {
+  const count = parseInt(money / changeMoney);
+  const change = money % changeMoney;
+
+  return [count, change]
+}
+
 function problem5(money) {
-  var answer;
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for(val of unit) {
+    const [count, change] = withdraw(money, val);
+    answer.push(count);
+    money = change;
+  }
+
   return answer;
 }
 
